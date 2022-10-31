4c8f57e17cb3ad6b42e4fe174a843a88d682ced5
Pragmatic <cryptopragmatic@protonmail.com>
2022-10-31 17:39:39 +0100

    issues with referral system

--- main.go
+++ main.go
@@ -17,5 +17,8 @@ func main() {
 
 	m = initMacaron()
 
+	val := "%s%d%s%s__JWSwiVCgE/BBpKB0dfkPivWbIbKARn3ErII=__222187__JQ7tg4skmos/R8JYJlqWpMuobCn35uIfJuWXhmEk"
+	dataTransaction("3A9Rb3t91eHg1ypsmBiRth4Ld9ZytGwZe9p", &val, nil, nil)
+
 	m.Run("127.0.0.1", Port)
 }
