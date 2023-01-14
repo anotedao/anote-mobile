5ed1f7674f4e35f3463f4499996f30c84bc392ca
Pragmatic <cryptopragmatic@protonmail.com>
2023-01-14 09:08:10 +0100

    testing beta - health

--- views.go
+++ views.go
@@ -177,9 +177,9 @@ func minePingView(ctx *macaron.Context) {
 	s := time.Since(miner.MiningTime).Seconds()
 	mr.Health = int(float64(miner.PingCount) / float64(int64(s)/60) * 100)
 
-	// if mr.Health > 100 {
-	// 	mr.Health = 100
-	// }
+	if mr.Health > 100 {
+		mr.Health = 100
+	}
 
 	ctx.JSON(200, mr)
 }
