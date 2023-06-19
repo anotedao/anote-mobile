bc14d7258efe7806aaa4a4aed13f28b127146dc5
Pragmatic <cryptopragmatic@protonmail.com>
2023-06-19 04:45:51 +0200

    testing

--- helpers.go
+++ helpers.go
@@ -737,7 +737,9 @@ func getBasicAmount(amount uint64) uint64 {
 	ba := uint64(0)
 	stats := getStats()
 
-	ba = uint64(float64(amount) / float64((uint64(stats.ActiveUnits) + uint64(stats.ActiveReferred)/4)))
+	ba = uint64(float64(amount) / float64((float64(stats.ActiveUnits) + float64(stats.ActiveReferred)/4)))
+
+	// float64((float64(totalt.Balance) / float64(uint64(stats.ActiveUnits)+uint64(stats.ActiveReferred/4)))) / MULTI8
 
 	return ba
 }
