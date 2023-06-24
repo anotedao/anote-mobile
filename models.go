e61be452a4d653ce2a801f39ac8431dfbc567da2
Pragmatic <cryptopragmatic@protonmail.com>
2023-06-25 01:24:15 +0200

    testing default

--- models.go
+++ models.go
@@ -18,7 +18,7 @@ type Miner struct {
 	gorm.Model
 	Address                string `gorm:"size:255;uniqueIndex"`
 	LastNotification       time.Time
-	LastNotificationWeekly time.Time `gorm:"default:2023-06-17 23:00:00.797487649+00:00"`
+	LastNotificationWeekly time.Time `gorm:"default:'2023-06-17 23:00:00.797487649+00:00'"`
 	TelegramId             int64
 	MiningHeight           int64
 	MiningTime             time.Time
