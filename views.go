336ab74564070ab8643c7664a3fe676c7a1e661a
Pragmatic <cryptopragmatic@protonmail.com>
2023-06-30 01:53:21 +0200

    unique address

--- views.go
+++ views.go
@@ -417,6 +417,7 @@ func saveTelegram(ctx *macaron.Context) {
 			result = db.FirstOrCreate(m, &Miner{Address: tids})
 		} else if strings.HasPrefix(ap, "3A") {
 			result = db.FirstOrCreate(m, &Miner{Address: tids})
+			m.Address = ap
 		} else {
 			refid, err := strconv.Atoi(ap)
 			if err != nil {
