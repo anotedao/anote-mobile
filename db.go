7fe2da8946a6dba90901d1c7b05e9ececa26cb11
Pragmatic <cryptopragmatic@protonmail.com>
2023-09-03 22:01:23 +0200

    back to sqlite

--- db.go
+++ db.go
@@ -3,15 +3,27 @@ package main
 import (
 	"log"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func initDb() *gorm.DB {
-	db, err := gorm.Open(postgres.Open(conf.DSN), &gorm.Config{})
+	var db *gorm.DB
+	var err error
+	// dbconf := gorm.Config{
+	// 	SkipDefaultTransaction: true,
+	// 	PrepareStmt:            true,
+	// }
+	dbconf := gorm.Config{}
+
+	dbconf.Logger = logger.Default.LogMode(logger.Error)
+
+	db, err = gorm.Open(sqlite.Open("mobile.db"), &dbconf)
 
 	if err != nil {
 		log.Println(err)
+		logTelegram(err.Error())
 	}
 
 	if err := db.AutoMigrate(&Miner{}, &IpAddress{}, &KeyValue{}); err != nil {
