49049877459f87a4bfc53bbb6cdf19334f2bb3cc
Pragmatic <cryptopragmatic@protonmail.com>
2023-09-06 01:07:32 +0200

    testing fix

--- models.go
+++ models.go
@@ -74,7 +74,7 @@ func getMiner(addr string) *Miner {
 
 func getMinerTel(tid int64) *Miner {
 	mnr := &Miner{}
-	db.FirstOrCreate(mnr, &Miner{TelegramId: tid})
+	db.First(mnr, &Miner{TelegramId: tid})
 
 	return mnr
 }
