7bc1791249e5d5218ca27aff64c7b79666f04dba
Pragmatic <cryptopragmatic@protonmail.com>
2023-09-27 02:55:04 +0200

    pprof

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
-	// _ "net/http/pprof"
+	_ "net/http/pprof"
 
 	"gopkg.in/macaron.v1"
 	"gorm.io/gorm"
@@ -24,9 +26,9 @@ var cch *Cache
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	// go func() {
-	// 	fmt.Println(http.ListenAndServe("localhost:6060", nil))
-	// }()
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
 	conf = initConfig()
 
