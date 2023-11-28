53ed74caf30beedf21ed8524ee1a33697e0bfe94
Pragmatic <cryptopragmatic@protonmail.com>
2023-11-28 21:35:03 +0100

    stop adding to tg

--- monitor.go
+++ monitor.go
@@ -30,11 +30,6 @@ func (m *Monitor) sendNotifications() {
 			log.Printf("Notification Weekly: %s %d", miner.Address, counter)
 			counter++
 		}
-
-		// if m.isSendingBattery(miner) {
-		// 	sendNotificationBattery(miner)
-		// 	log.Printf("Notification battery: %s", miner.Address)
-		// }
 	}
 }
 
@@ -79,30 +74,6 @@ func (m *Monitor) isSendingWeekly(miner *Miner, limit int64) bool {
 	return false
 }
 
-func (m *Monitor) isSendingBattery(miner *Miner) bool {
-	// health := int(getIpFactor(miner, true, uint64(m.Height), 2) * 100)
-
-	// if health > 100 {
-	// 	health = 100
-	// } else if health < 0 {
-	// 	health = 0
-	// }
-
-	// if miner.ID != 0 &&
-	// 	miner.BatteryNotification &&
-	// 	health < 100 &&
-	// 	time.Since(miner.MiningTime) > (time.Minute*5) &&
-	// 	miner.TelegramId != 0 {
-
-	// 	miner.BatteryNotification = false
-	// 	db.Save(miner)
-
-	// 	return true
-	// }
-
-	return false
-}
-
 func (m *Monitor) minerExists(telId int64) bool {
 	for _, mnr := range m.Miners {
 		if int64(mnr.TelegramId) == telId {
@@ -111,45 +82,6 @@ func (m *Monitor) minerExists(telId int64) bool {
 	}
 
 	return false
-}
-
-func (m *Monitor) checkMined() {
-	var err error
-	m.NewBalanceTelegram, err = getBalance(TelegramAddress)
-	if err == nil && m.NewBalanceTelegram > m.OldBalanceTelegram {
-		diff := m.NewBalanceTelegram - m.OldBalanceTelegram
-		if diff > 0 {
-			ba := getBasicAmount(diff)
-
-			log.Printf("Telegram Basic: %d", ba)
-
-			m.OldBalanceTelegram = m.NewBalanceTelegram
-
-			ks := &KeyValue{Key: "oldBalanceTelegram"}
-			db.FirstOrCreate(ks, ks)
-			ks.ValueInt = m.OldBalanceTelegram
-			err := db.Save(ks).Error
-			if err != nil {
-				log.Println(err)
-				logTelegram(err.Error())
-			}
-
-			for _, mnr := range m.Miners {
-				if m.Height-uint64(mnr.MiningHeight) <= 1410 {
-					mnr.MinedTelegram += uint64(float64(ba) * getMiningFactor(mnr))
-					err = db.Save(mnr).Error
-					if err != nil {
-						log.Println(err)
-						logTelegram(err.Error())
-					}
-				}
-			}
-
-			log.Printf("New Telegram Amount: %d", diff)
-		}
-	} else if err != nil {
-		log.Println(err)
-	}
 }
 
 func (m *Monitor) start() {
@@ -172,24 +104,8 @@ func (m *Monitor) start() {
 	}
 }
 
-func (m *Monitor) start2() {
-	m.loadMiners()
-
-	ks := &KeyValue{Key: "oldBalanceTelegram"}
-	db.FirstOrCreate(ks, ks)
-
-	m.OldBalanceTelegram = uint64(ks.ValueInt)
-
-	for {
-		m.Height = getHeight()
-		m.checkMined()
-		time.Sleep(time.Second * 120)
-	}
-}
-
 func initMonitor() *Monitor {
 	m := &Monitor{}
 	go m.start()
-	go m.start2()
 	return m
 }
