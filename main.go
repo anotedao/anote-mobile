9d24ea64a92cdf78031384858a1df0813eed1b42
Pragmatic <cryptopragmatic@protonmail.com>
2023-12-14 16:22:42 +0100

    sent tg notifications

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/url"
 
 	// _ "net/http/pprof"
 
@@ -40,29 +39,6 @@ func main() {
 	pc = initPriceClient()
 
 	cch = initCache()
-
-	go func() {
-		var miners []*Miner
-		db.Where("mined_telegram > ?", Fee).Find(&miners)
-		log.Println(len(miners))
-
-		msg := `Please notice that you have anotes accrued on this bot!
-		
-To withdraw them, click here -> /withdraw or do it from menu on this bot.
-	
-If you haven't connected the wallet yet, open it (app.anotedao.com), click Connect Telegram on the bottom of the wallet and do withdraw after that.
-	
-After withdrawal, you will receive your anotes once a day, when you enter daily mining code. Happy mining! 🚀`
-
-		msg = url.QueryEscape(msg)
-
-		// telegramNotification(963770508, msg)
-
-		for i, m := range miners {
-			telegramNotification(m.TelegramId, msg)
-			log.Printf("Sent TG notification: %d", i)
-		}
-	}()
 
 	mac.Run("127.0.0.1", Port)
 }
